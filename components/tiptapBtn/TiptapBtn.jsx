27932e026f6cc00fe71b60ad4fd626cd15dda689
unknown <godhustler90956@gmail.com>
2023-10-11 13:14:16 +0530

    01:14pm

--- components/tiptapBtn/TiptapBtn.jsx
+++ components/tiptapBtn/TiptapBtn.jsx
@@ -1,41 +1,72 @@
 "use client"
+import { useState } from "react"
 import styles from "./tiptapbtn.module.css"
 import {
     BsQuote,
     BsTypeBold,
     BsListUl,
     BsCodeSlash,
-    BsCodeSquare
+    BsCodeSquare,
+    BsFonts
 } from "react-icons/bs"
+import { AiOutlineHighlight } from "react-icons/ai"
+import { markHighlight, fontFamily } from "@/data/data"
 
 
+const initialState = {
+    toggleFont: false,
+    toggleHighlight: false,
+
+}
 const TiptapBtn = ({ editor }) => {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //       showList: false,
-    //       defaultSelectText: "Please select an option",
-    //       countryList: [
-    //         { id: 1, name: "Australia" },
-    //         { id: 2, name: "Brazil" },
-    //         { id: 3, name: "China" },
-    //         { id: 4, name: "Denmark" },
-    //         { id: 5, name: "Egypt" },
-    //         { id: 6, name: "Finland" },
-    //         { id: 7, name: "Ghana" },
-    //         { id: 8, name: "Hungary" },
-    //         { id: 9, name: "India" },
-    //         { id: 10, name: "Japan" }
-    //       ]
-    //     };
-    //   }
+    const [state, setstate] = useState(initialState)
+
+
+
+    const handleFToggle = () => {
+        setstate(prevState => (
+            {
+                ...prevState,
+                toggleFont: !prevState.toggleFont
+            }
+        ))
+
+        if (state.toggleHighlight) {
+            setstate(prevState => (
+                {
+                    ...prevState,
+                    toggleHighlight: !prevState.toggleHighlight
+                }
+            ))
+        }
+    }
+
+
+    const handleHToggle = () => {
+        setstate(prevState => (
+            {
+                ...prevState,
+                toggleHighlight: !prevState.toggleHighlight
+            }
+        ))
+
+        if (state.toggleFont) {
+            setstate(prevState => (
+                {
+                    ...prevState,
+                    toggleFont: !prevState.toggleFont
+                }
+            ))
+        }
+    }
+
 
     return (
-        <>
+        <div className={styles.container}>
             <button
                 onClick={() => editor.chain().focus().toggleBlockquote().run()}
-                className={editor.isActive('blockquote') ? 'is-active' : `${styles.btn}`}
+                className={`${editor.isActive('blockquote') ? `${styles.isactive}` : `${styles.btn}`}`}
             >
                 <BsQuote className={styles.icons} />
             </button>
@@ -47,25 +78,19 @@ const TiptapBtn = ({ editor }) => {
             </button>
             <button
                 onClick={() => editor.chain().focus().toggleBulletList().run()}
-                className={editor.isActive('bulletList') ? 'is-active' : `${styles.btn}`}
+                className={`${editor.isActive('bulletList') ? `${styles.isactive}` : `${styles.btn}`}`}
             >
                 < BsListUl className={styles.icons} />
             </button>
             <button
                 onClick={() => editor.chain().focus().toggleCode().run()}
-                className={editor.isActive('code') ? 'is-active' : `${styles.btn}`}
+                className={`${editor.isActive('code') ? `${styles.isactive}` : `${styles.btn}`}`}
             >
                 <BsCodeSlash className={styles.icons} />
-            </button>
-            {/* <button
-                onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-                className={editor.isActive('codeBlock') ? 'is-active' : `${styles.btn}`}
-            >
-                < BsCodeSquare className={styles.icons} />
-            </button> */}
+            </button >
             <button
                 onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-                className={editor.isActive('codeBlock') ? 'is-active' : ''}
+                className={`${editor.isActive('codeBlock') ? `${styles.isactive}` : `${styles.btn}`}`}
             >
                 < BsCodeSquare className={styles.icons} />
             </button>
@@ -77,80 +102,58 @@ const TiptapBtn = ({ editor }) => {
                 data-testid="setColor"
             />
 
-            <button
-                onClick={() => editor.chain().focus().setFontFamily('Inter').run()}
-                className={editor.isActive('textStyle', { fontFamily: 'Inter' }) ? 'is-active' : ''}
-            >
-                Inter
-            </button>
-            <button
-                onClick={() => editor.chain().focus().setFontFamily('Comic Sans MS, Comic Sans').run()}
-                className={
-                    editor.isActive('textStyle', { fontFamily: 'Comic Sans MS, Comic Sans' })
-                        ? 'is-active'
-                        : ''
-                }
-            >
-                Comic Sans
-            </button>
-            <button
-                onClick={() => editor.chain().focus().setFontFamily('serif').run()}
-                className={editor.isActive('textStyle', { fontFamily: 'serif' }) ? 'is-active' : ''}
-            >
-                serif
-            </button>
-            <button
-                onClick={() => editor.chain().focus().setFontFamily('monospace').run()}
-                className={editor.isActive('textStyle', { fontFamily: 'monospace' }) ? 'is-active' : ''}
-            >
-                monospace
-            </button>
-            <button
-                onClick={() => editor.chain().focus().setFontFamily('cursive').run()}
-                className={editor.isActive('textStyle', { fontFamily: 'cursive' }) ? 'is-active' : ''}
-            >
-                cursive
-            </button>
-            <button onClick={() => editor.chain().focus().unsetFontFamily().run()}>
-                unsetFontFamily
-            </button>
+            <div className={styles.select} >
+                <span onClick={handleFToggle} className={styles.span}><BsFonts /></span>
+                <ul className={`${state.toggleFont ? `${styles.isulactive} ${styles.ul}` : `${styles.ul}`}`}>
 
-            <button
-                onClick={() => editor.chain().focus().toggleHighlight().run()}
-                className={editor.isActive('highlight') ? 'is-active' : ''}
-            >
-                toggleHighlight
-            </button>
+                    {fontFamily.map((item, idx) => (
+                        <li
+                            key={idx}
+                            onClick={() => editor.chain().focus().setFontFamily(item.fontFamily).run()}
+                            className={styles.li}
+                        >{item.name}</li>
+                    ))}
 
 
-            <button
-                onClick={() => editor.chain().focus().toggleHighlight({ color: '#ffc078' }).run()}
-                className={editor.isActive('highlight', { color: '#ffc078' }) ? 'is-active' : ''}
-            >
-                orange
-            </button>
-            <button
-                onClick={() => editor.chain().focus().toggleHighlight({ color: '#8ce99a' }).run()}
-                className={editor.isActive('highlight', { color: '#8ce99a' }) ? 'is-active' : ''}
-            >
-                green
-            </button>
-            <button
-                onClick={() => editor.chain().focus().toggleHighlight({ color: '#74c0fc' }).run()}
-                className={editor.isActive('highlight', { color: '#74c0fc' }) ? 'is-active' : ''}
-            >
-                blue
-            </button>
-            <button
-                onClick={() => editor.chain().focus().toggleHighlight({ color: '#b197fc' }).run()}
-                className={editor.isActive('highlight', { color: '#b197fc' }) ? 'is-active' : ''}
-            >
-                purple
-            </button>
 
-   
+                    <li
+                        className={styles.li}
+                        onClick={() => editor.chain().focus().unsetFontFamily().run()}
+                    > unsetFontFamily</li>
+                </ul>
+            </div>
 
-        </>
+
+
+            <div className={styles.select}  >
+                <span onClick={handleHToggle} className={styles.span}><AiOutlineHighlight /></span>
+                <ul className={`${state.toggleHighlight ? `${styles.isulactive} ${styles.ul}` : `${styles.ul}`}`}>
+
+
+
+                    <li
+                        onClick={() => editor.chain().focus().toggleHighlight().run()}
+                        className={styles.li}
+                    >toggleHighlight</li>
+                    {markHighlight.map((item, idx) => (
+                        <li
+                            key={idx}
+                            onClick={() => editor.chain().focus().toggleHighlight({ color: item.color }).run()}
+                            className={`${state.toggleHighlight ? `${styles.isliactive} ${styles.li}` : `${styles.li}`}`}
+                        > {item.name} </li>
+                    ))}
+
+
+
+                </ul>
+            </div>
+
+
+
+
+
+
+        </div>
     )
 }
 
---
+++ data/data.js
@@ -0,0 +1,16 @@
+const markHighlight = [
+  { name: "orange", color: "#ffc078" },
+  { name: " green", color: "#8ce99a" },
+  { name: "blue", color: "#74c0fc" },
+  { name: "purple", color: "#b197fc" },
+];
+
+const fontFamily = [
+  { name: "Inter", fontFamily: "Inter" },
+  { name: "Comic Sans", fontFamily: "Comic Sans MS, Comic Sans" },
+  { name: "serif", fontFamily: "serif" },
+  { name: "monospace", fontFamily: "monospace" },
+  { name: "cursive", fontFamily: "cursive" },
+];
+
+export { markHighlight, fontFamily };
