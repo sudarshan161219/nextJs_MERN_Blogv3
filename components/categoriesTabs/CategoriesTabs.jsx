427eb4943327fd895f0ef0aa542e1c50646b4107
unknown <godhustler90956@gmail.com>
2023-12-30 20:14:18 +0530

    08:14pm

--- components/categoriesTabs/CategoriesTabs.jsx
+++ components/categoriesTabs/CategoriesTabs.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+"use client"
+import { useState, useRef, useEffect } from 'react'
 import styles from "./categoriesTabs.module.css"
 import Link from 'next/link';
 import Image from 'next/image';
+import Slider from "react-slick";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai"
+import { motion } from "framer-motion"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+function getWindowSize() {
+    const { innerWidth, innerHeight } = window;
+    return { innerWidth, innerHeight };
+}
 const CategoriesTabs = () => {
-
+    const [wsize, setWsize] = useState(0)
+    const [windowSize, setWindowSize] = useState(getWindowSize());
     const blogCategories = [
         {
             name: "Lifestyle",
@@ -108,21 +119,82 @@ const CategoriesTabs = () => {
         },
     ];
 
+    const sliderRef = useRef(null);
+
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    function resizefn() {
+        if (windowSize.innerWidth <= 500) {
+            setWsize(1)
+        }
+        if (windowSize.innerWidth >= 500) {
+            setWsize(2)
+        }
+        if (windowSize.innerWidth >= 700) {
+            setWsize(4)
+        }
+        if (windowSize.innerWidth >= 1020) {
+            setWsize(4)
+        }
+        if (windowSize.innerWidth >= 1280) {
+            setWsize(5)
+        }
+    }
+    useEffect(() => {
+        function handleWindowResize() {
+            setWindowSize(getWindowSize());
+        }
+        resizefn()
+        window.addEventListener('resize', handleWindowResize);
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, [resizefn]);
+
+
+    const settings = {
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: wsize,
+        slidesToScroll: 1
+    };
+
+    const goToNext = () => {
+        sliderRef.current.slickNext();
+    };
+
+    const goToPrev = () => {
+        sliderRef.current.slickPrev();
+    };
 
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Popular Categories</h1>
 
-            <div className={styles.categorylist}>
-                {
-                    blogCategories.map((item, idx) => (
-                        <Link key={idx} href={item.herf} className={styles.list}>
-                            <strong className={styles.listTitle} >{item.name}</strong>
-                            <Image className={styles.listimg} src={item.img} alt={item.name} width={40} height={40} />
-                        </Link>
-                    ))
-                }
+            <div className='relative '>
+                <button className={styles.iconBtn1} onClick={goToPrev}>< AiOutlineArrowLeft className={styles.icon} /></button>
+
+                <Slider ref={sliderRef} {...settings}>
+                    {
+                        blogCategories.map((item, idx) => (
+
+                            <Link key={idx} href={item.herf} className={styles.list}>
+                                <motion.div
+                                    className={styles.item}
+                                >
+                                    <strong className={styles.listTitle} >{item.name}</strong>
+                                    <Image className={styles.listimg} src={item.img} alt={item.name} width={40} height={40} />
+                                </motion.div>
+                            </Link>
+                        ))
+                    }
+                </Slider>
+                {/* <div className={styles.btnContainer}> */}
+                <button className={styles.iconBtn2} onClick={goToNext}><AiOutlineArrowRight className={styles.icon} /></button>
+                {/* </div> */}
             </div>
+
         </div>
     )
 }
