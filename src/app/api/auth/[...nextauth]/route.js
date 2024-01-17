e3fe7fa57e1fe1b899dd481ff737bb24af5f8430
unknown <godhustler90956@gmail.com>
2024-01-17 18:51:42 +0530

    07:28pm

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import { authOptions } from "../../../../utils/auth.js";
 
-
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
